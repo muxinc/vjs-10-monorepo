74dea64dddee7d5a1227a7aaa19ecb6f72ea5130
Christian Pillsbury <cpillsbury@mux.com>
2025-08-20 15:28:04 -0500

    refactor(html): implement hook-style component architecture for PlayButton and MuteButton (gradual migration to more shareable with React).

--- packages/html/html/src/index.ts
+++ packages/html/html/src/index.ts
@@ -2,8 +2,8 @@ export * as MediaProvider from './media-provider.js';
 export * as MediaThemeDefault from './skins/media-skin-default.js';
 
 // New hook-style components
-export { PlayButton } from './components/PlayButton.js';
-export { MuteButton } from './components/MuteButton.js';
+export { PlayButton } from './components/media-play-button.js';
+export { MuteButton } from './components/media-mute-button.js';
 
 export function defineVjsPlayer() {
   /** @TODO - Reimplement me (at least as a POC) (CJP) */
