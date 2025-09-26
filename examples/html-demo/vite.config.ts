c49bfee28cc060d296f13478dcea7af31f9e3276
Rahim <rahim.alwer@gmail.com>
2025-09-26 12:03:25 +1000

    done

---
+++ examples/html-demo/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config
+export default defineConfig({
+  server: {
+    port: 5174,
+  },
+  optimizeDeps: {
+    exclude: ['@vjs/html'],
+  },
+});
--- examples/react-demo/vite.config.ts
+++ examples/react-demo/vite.config.ts
@@ -1,19 +1,15 @@
-import type { UserConfig } from 'vite';
+import { defineConfig } from 'vite';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default {
+// https://vitejs.dev/config
+export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: '0.0.0.0', // Listen on any interface
     port: 5173,
-    watch: {
-      ignored: [],
-    },
   },
   optimizeDeps: {
-    exclude: ['@vjs/react'], // Don’t prebundle, keeps rebuilds fast.
+    exclude: ['@vjs/react'],
   },
-} satisfies UserConfig;
+});
