74b94518d5f20e9ef4ee2d28559944b31ddbeea5
Christian Pillsbury <cpillsbury@mux.com>
2025-09-30 10:36:55 -0700

    build: Attempt to improve css compiler code WIP - DO NOT MERGE

--- packages/build-tools/tailwind-css-compiler/src/tailwind-ast/segment.ts
+++ packages/build-tools/tailwind-css-compiler/src/tailwind-ast/segment.ts
@@ -1,18 +1,18 @@
-const BACKSLASH = 0x5c
-const OPEN_CURLY = 0x7b
-const CLOSE_CURLY = 0x7d
-const OPEN_PAREN = 0x28
-const CLOSE_PAREN = 0x29
-const OPEN_BRACKET = 0x5b
-const CLOSE_BRACKET = 0x5d
-const DOUBLE_QUOTE = 0x22
-const SINGLE_QUOTE = 0x27
+const BACKSLASH = 0x5c;
+const OPEN_CURLY = 0x7b;
+const CLOSE_CURLY = 0x7d;
+const OPEN_PAREN = 0x28;
+const CLOSE_PAREN = 0x29;
+const OPEN_BRACKET = 0x5b;
+const CLOSE_BRACKET = 0x5d;
+const DOUBLE_QUOTE = 0x22;
+const SINGLE_QUOTE = 0x27;
 
 // This is a shared buffer that is used to keep track of the current nesting level
 // of parens, brackets, and braces. It is used to determine if a character is at
 // the top-level of a string. This is a performance optimization to avoid memory
 // allocations on every call to `segment`.
-const closingBracketStack = new Uint8Array(256)
+const closingBracketStack = new Uint8Array(256);
 
 /**
  * This splits a string on a top-level character.
@@ -26,61 +26,64 @@ const closingBracketStack = new Uint8Array(256)
  *        x              x  x    ╰──────── Split because top-level
  *        ╰──────────────┴──┴───────────── Ignored b/c inside >= 1 levels of parens
  */
-export function segment(input: string, separator: string): string[] {
+export function segment(
+  input: string,
+  separator: string
+): [string] | [string, string] | [string, string, string] | string[] {
   // SAFETY: We can use an index into a shared buffer because this function is
   // synchronous, non-recursive, and runs in a single-threaded environment.
-  let stackPos = 0
-  let parts: string[] = []
-  let lastPos = 0
-  let len = input.length
+  let stackPos = 0;
+  let parts: string[] = [];
+  let lastPos = 0;
+  let len = input.length;
 
-  let separatorCode = separator.charCodeAt(0)
+  let separatorCode = separator.charCodeAt(0);
 
   for (let idx = 0; idx < len; idx++) {
-    let char = input.charCodeAt(idx)
+    let char = input.charCodeAt(idx);
 
     if (stackPos === 0 && char === separatorCode) {
-      parts.push(input.slice(lastPos, idx))
-      lastPos = idx + 1
-      continue
+      parts.push(input.slice(lastPos, idx));
+      lastPos = idx + 1;
+      continue;
     }
 
     switch (char) {
       case BACKSLASH:
         // The next character is escaped, so we skip it.
-        idx += 1
-        break
+        idx += 1;
+        break;
       // Strings should be handled as-is until the end of the string. No need to
       // worry about balancing parens, brackets, or curlies inside a string.
       case SINGLE_QUOTE:
       case DOUBLE_QUOTE:
         // Ensure we don't go out of bounds.
         while (++idx < len) {
-          let nextChar = input.charCodeAt(idx)
+          let nextChar = input.charCodeAt(idx);
 
           // The next character is escaped, so we skip it.
           if (nextChar === BACKSLASH) {
-            idx += 1
-            continue
+            idx += 1;
+            continue;
           }
 
           if (nextChar === char) {
-            break
+            break;
           }
         }
-        break
+        break;
       case OPEN_PAREN:
-        closingBracketStack[stackPos] = CLOSE_PAREN
-        stackPos++
-        break
+        closingBracketStack[stackPos] = CLOSE_PAREN;
+        stackPos++;
+        break;
       case OPEN_BRACKET:
-        closingBracketStack[stackPos] = CLOSE_BRACKET
-        stackPos++
-        break
+        closingBracketStack[stackPos] = CLOSE_BRACKET;
+        stackPos++;
+        break;
       case OPEN_CURLY:
-        closingBracketStack[stackPos] = CLOSE_CURLY
-        stackPos++
-        break
+        closingBracketStack[stackPos] = CLOSE_CURLY;
+        stackPos++;
+        break;
       case CLOSE_BRACKET:
       case CLOSE_CURLY:
       case CLOSE_PAREN:
@@ -90,13 +93,13 @@ export function segment(input: string, separator: string): string[] {
           // position is only ever incremented after writing to it. Meaning that
           // the buffer can be dirty for the next use and still be correct since
           // reading/writing always starts at position `0`.
-          stackPos--
+          stackPos--;
         }
-        break
+        break;
     }
   }
 
-  parts.push(input.slice(lastPos))
+  parts.push(input.slice(lastPos));
 
-  return parts
+  return parts as [string] | [string, string] | [string, string, string] | string[];
 }
