e0a46de0ab56199afd37b24ebfce93ee85e2b699
Sam Potts <sam@potts.es>
2025-10-25 17:11:00 +1100

    fix: add aspect-ratio to demos (#136)

--- examples/html-demo/src/main.ts
+++ examples/html-demo/src/main.ts
@@ -7,8 +7,8 @@ defineVjsPlayer();
 document.body.innerHTML = /* html */ `
   <media-provider>
     <div style="display: grid; place-items: center; min-height: 100vh;">
-      <media-skin-frosted style="border-radius: 2rem; width: 100%; max-width: 800px; margin: 2rem auto;">
-        <video slot="media" src="https://stream.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/high.mp4" muted></video>
+      <media-skin-frosted style="border-radius: 2rem; width: 100%; max-width: 960px; margin: 2rem auto; aspect-ratio: 16/9">
+        <video slot="media" src="https://stream.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/high.mp4" muted playsinline poster="https://image.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/thumbnail.webp"></video>
       </media-skin-frosted>
     </div>
   </media-provider>
